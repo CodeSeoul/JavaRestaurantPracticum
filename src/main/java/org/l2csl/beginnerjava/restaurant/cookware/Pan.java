291207111237f30d5ea610e42f75b268e946f490
Bryan Beege Berry <draco4king@gmail.com>
2016-12-01 17:27:34 +0900

    Setup cookware subclasses to cook appropriately.

--- src/main/java/org/l2csl/beginnerjava/restaurant/cookware/Pan.java
+++ src/main/java/org/l2csl/beginnerjava/restaurant/cookware/Pan.java
@@ -1,8 +1,10 @@
 package org.l2csl.beginnerjava.restaurant.cookware;
 
+import org.l2csl.beginnerjava.restaurant.Ingredient;
+
 /**
  * Created by draco on 11/24/2016.
  */
 public class Pan extends BaseCookware {
-    // TODO: Override letCook appropriately for this cookware
+    protected final String COOK_STATE = Ingredient.SAUTEED;
 }
