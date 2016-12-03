c045c7744f5ed1a4cba5298372dad3e05379971b
Bryan Beege Berry <draco4king@gmail.com>
2016-12-03 18:26:56 +0900

    Added test for test folder.

---
+++ src/test/java/org/l2csl/beginnerjava/restaurant/IngredientTest.java
@@ -0,0 +1,42 @@
+package org.l2csl.beginnerjava.restaurant;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by draco on 12/3/2016.
+ */
+class IngredientTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void takePortion() {
+
+    }
+
+    @Test
+    void addPortion() {
+
+    }
+
+    @Test
+    void cook() {
+
+    }
+
+    @Test
+    void toString() {
+
+    }
+
+    @Test
+    void equals() {
+
+    }
+
+}
