ca5733b0696284c515b7ad4ca98fec579bd9aacd
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2021-01-24 18:22:29 +0900

    chore: Fixed Appliance TODOs to be clearer for cookware

--- src/main/java/org/l2csl/beginnerjava/restaurant/appliance/Stove.java
+++ src/main/java/org/l2csl/beginnerjava/restaurant/appliance/Stove.java
@@ -6,10 +6,12 @@ import org.l2csl.beginnerjava.restaurant.cookware.ICookware;
  * Created by draco on 11/24/2016.
  */
 public class Stove extends BaseAppliance {
+
     @Override
     public void placeCookware(ICookware cookware) {
         // TODO: What cookware should a stove accept? Throw an exception if the wrong cookware is given
         // TODO: Is there a capacity on an stove? Enforce it with an exception
+        // TODO: After the above, call the superclass's placeCookware function
         // Hint: Once you have this throw an exception, you'll need to make changes in other classes
     }
 }
